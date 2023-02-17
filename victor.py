25ee80c93731965e062e2b3300301a6ad723c78d
Doblepe <vicdobleperez@gmail.com>
2023-02-17 20:07:52 +0100

    adding some charts

--- victor.py
+++ victor.py
@@ -1,21 +1,34 @@
 import streamlit as st
-
-import plotly.express as px
-import matplotlib.pyplot as plt
 import yfinance as yf
- 
-#Selectbox 
-options = st.multiselect('Escoge la empresa de la que quieras ver sus acciones', ('Google', 'Tesla', 'Microsoft', 'Apple'))
-#Creamos una lista con los tickers a descargar
-#lista_precios = ['TSLA', 'GOOG', 'MSFT', 'AAPL']
-#Descargamos todos los tickers de la lista y sólo los datos que necesitamos
-data = yf.download(options, start="2012-01-01")['Adj Close']
+# #Selectbox 
+# dict = {"Google": 'GOOGL',
+#         "Tesla": "TSL",
+#         "Microsoft": "MSFT",
+#         "Apple": "AAPL"}
+
+options = st.multiselect('Escoge la empresa de la que quieras ver sus acciones', ('GOOGL', 'TSL', 'MSFT', 'AAPL'))
+
+#companies = ('GOOGL', 'TSL', 'MSFT', 'AAPL')
+
+
+def show_results(options): 
+    for opt in options:
+        st.write('Las empresa seleccionada es', opt)
+        data = yf.download(opt, start="2012-01-01")['Adj Close']
+        st.line_chart(data)
+
+
+
+show_results(options)
+
+
+
+#data = yf.download(companies, start="2012-01-01")['Adj Close']
 
  
 
-print(type (options))
-print(type(data))
+
+
 # data = yf.download(options, start="2012-01-01")['Adj Close']
 
-st.line_chart(data)
-st.line_chart(options)
+#st.line_chart(data)
