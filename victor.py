3254b90c867c3b882ff9dd58e648567a96aa0b5b
Doblepe <vicdobleperez@gmail.com>
2023-02-18 08:32:57 +0100

    All in one box

--- victor.py
+++ victor.py
@@ -1,22 +1,32 @@
 import streamlit as st
 import yfinance as yf
 # #Selectbox 
+#TODO: FIX with de dict to set the full companies name
 # dict = {"Google": 'GOOGL',
 #         "Tesla": "TSL",
 #         "Microsoft": "MSFT",
 #         "Apple": "AAPL"}
-
-options = st.multiselect('Escoge la empresa de la que quieras ver sus acciones', ('GOOGL', 'TSL', 'MSFT', 'AAPL'))
+date = st.date_input
+options = st.multiselect('Escoge la empresa de la que quieras ver sus acciones', ('GOOGL', 'TSL', 'MSFT', 'AAPL','AMZN'))
 
 #companies = ('GOOGL', 'TSL', 'MSFT', 'AAPL')
-
-
-def show_results(options): 
-    for opt in options:
-        st.write('Las empresa seleccionada es', opt)
-        data = yf.download(opt, start="2012-01-01")['Adj Close']
+def show_results(options):
+    #transformo las options a list para poder descargarla
+    options = list(options)
+    if options.__len__() == 0:
+        st.write('Todavía no has seleccionado ninguna empresa')
+    else:
+        data = yf.download(options, start="2012-01-01")['Adj Close']
         st.line_chart(data)
+    
 
+
+#def show_several_boxes(options):
+    # for opt in options:
+    #     st.write('Las empresa seleccionada es', opt)
+    #     data = yf.download(opt, start="2012-01-01")['Adj Close']
+    #     print(data)
+         
 
 
 show_results(options)
